c019aeb4a86169da28a205100175db162ff24189
Joe Leinsdorf <leinsdorf@gmail.com>
2015-08-03 16:53:06 -0600

    v0.1 complete and under review

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,34 @@
 $(function() {
 
+  $('.overlay').hide();
+  $('.close').click(hideModals);
+  $('#music-link').click(showMusic);
+  $('#dates-link').click(showDates);
+  $('#photos-link').click(showPhotos);
+  $('#contact-link').click(showContact);
+
 
 });
+
+var hideModals = function() {
+  $('.overlay').fadeOut(1000);
+};
+
+var showMusic = function() {
+  hideModals();
+  $('#music-overlay').fadeIn(700);
+};
+
+var showDates = function() {
+  hideModals();
+  $('#dates-overlay').fadeIn(700);
+};
+
+var showPhotos = function() {
+  hideModals();
+  $('#photos-overlay').fadeIn(700);
+};
+var showContact = function() {
+  hideModals();
+  $('#contact-overlay').fadeIn(700);
+};
