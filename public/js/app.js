9023f971e71ca8b2212663da6927015b07d2ce58
Joe Leinsdorf <leinsdorf@gmail.com>
2016-01-03 14:26:53 -0500

    stuffs

--- public/js/app.js
+++ public/js/app.js
@@ -1,34 +1,21 @@
 $(function() {
 
   $('.overlay').hide();
-  $('.close').click(hideModals);
-  $('#music-link').click(showMusic);
-  $('#dates-link').click(showDates);
-  $('#photos-link').click(showPhotos);
-  $('#connect-link').click(showConnect);
-
+  $('.close').click(hideModal);
+  $('.main-img').click(showModal);
+  $('.logo-purple').hover(function() {
+    $(this).css('opacity', 0)
+  }, function() {
+    $(this).css('opacity', 1)
+  });  
 
 });
 
-var hideModals = function() {
+var hideModal = function() {
   $('.overlay').fadeOut(200);
 };
 
-var showMusic = function() {
-  hideModals();
-  $('#music-overlay').fadeIn(700);
-};
-
-var showDates = function() {
-  hideModals();
-  $('#dates-overlay').fadeIn(700);
-};
-
-var showPhotos = function() {
-  hideModals();
-  $('#photos-overlay').fadeIn(700);
-};
-var showConnect = function() {
-  hideModals();
-  $('#contact-overlay').fadeIn(700);
+var showModal = function() {
+  $('.logo-purple').css('opacity', 1, 'slow');
+  $('#modal').fadeIn(700);
 };
